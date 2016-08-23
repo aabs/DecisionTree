3a6eda77b14ccac5327967adc8ab189fa7bd41d6
andrew matthews <matthews.andrew@gmail.com>
2016-08-23 18:26:13 +1000

    Basic graph construction working

--- bdd/Collections.cs
+++ bdd/Collections.cs
@@ -18,8 +18,8 @@ namespace bdd
         /// for the actual graph which is the set of <see cref="GraphVertex{TVertexType}"/> 
         /// and <see cref="GraphEdge{TEdgeLabelType}"/>
         /// </remarks>
-        public GraphEdge<TVertexType, TEdgeLabelType> Root { get; set; }
-        public IEnumerable<GraphVertex<TVertexType, TEdgeLabelType>> Vertexs
+        public Edge<TVertexType, TEdgeLabelType> Root { get; set; }
+        public IEnumerable<Vertex<TVertexType, TEdgeLabelType>> Vertexs
         {
             get
             {
@@ -27,7 +27,7 @@ namespace bdd
             }
         }
 
-        private IEnumerable<GraphVertex<TVertexType, TEdgeLabelType>> GetAllVertexs(GraphVertex<TVertexType, TEdgeLabelType> n)
+        private IEnumerable<Vertex<TVertexType, TEdgeLabelType>> GetAllVertexs(Vertex<TVertexType, TEdgeLabelType> n)
         {
             yield return n;
             var childVertexs = n.Children.SelectMany(c => GetAllVertexs(c.TargetVertex));
@@ -38,15 +38,21 @@ namespace bdd
         }
     }
 
-    public class GraphVertex<TVertexType, TEdgeLabelType>
+    public class Vertex<TVertexType, TEdgeLabelType>
         where TVertexType : IEquatable<TVertexType>
         where TEdgeLabelType : IEquatable<TEdgeLabelType>
     {
-        public IEnumerable<GraphEdge<TVertexType, TEdgeLabelType>> Parents { get; }
+        public IEnumerable<Edge<TVertexType, TEdgeLabelType>> Parents { get; }
         public TVertexType Value { get; set; }
-        List<GraphEdge<TVertexType, TEdgeLabelType>> children = new List<GraphEdge<TVertexType, TEdgeLabelType>>();
+        List<Edge<TVertexType, TEdgeLabelType>> children = new List<Edge<TVertexType, TEdgeLabelType>>();
+        private string v;
 
-        public IEnumerable<GraphEdge<TVertexType, TEdgeLabelType>> Children
+        public Vertex(TVertexType v)
+        {
+            this.Value = v;
+        }
+
+        public IEnumerable<Edge<TVertexType, TEdgeLabelType>> Children
         {
             get
             {
@@ -54,31 +60,73 @@ namespace bdd
             }
         }
 
-        public GraphVertex<TVertexType, TEdgeLabelType> AddChild(TVertexType n, TEdgeLabelType e)
+        public Vertex<TVertexType, TEdgeLabelType> AddChild(TVertexType n, TEdgeLabelType e)
         {
             if (children.Any(c => c.LinkLabel.Equals(e)))
             {
                 throw new ApplicationException("Child link already exists.  Consider using Replace operation instead.");
             }
 
-            var graphVertex = new GraphVertex<TVertexType, TEdgeLabelType>
+            var graphVertex = new Vertex<TVertexType, TEdgeLabelType>(n);
+            children.Add(new Edge<TVertexType, TEdgeLabelType>
             {
-                Value = n
-            };
-            children.Add(new GraphEdge<TVertexType, TEdgeLabelType>
-                {
                 LinkLabel = e,
                 TargetVertex = graphVertex
-                });
+            });
             return graphVertex;
+        }
+
+        public Vertex<TVertexType, TEdgeLabelType> Child(TEdgeLabelType e)
+        {
+            return (from c in Children
+                    where c.LinkLabel.Equals(e)
+                    select c.TargetVertex).First();
+        }
+
+        public bool EquivalentTo(Vertex<TVertexType, TEdgeLabelType> sut2)
+        {
+            try
+            {
+                bool result = this.Value.Equals(sut2.Value);
+                result &= (Children.Count() == sut2.Children.Count());
+
+                foreach (var c in children)
+                {
+                    var v1 = Child(c.LinkLabel);
+                    var v2 = sut2.Child(c.LinkLabel);
+                    result &= v1.EquivalentTo(v2);
+                }
+
+                return result;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public TVertexType this[TEdgeLabelType index]
+        {
+            get
+            {
+                try
+                {
+                    return Child(index).Value;
+                }
+                catch (Exception e)
+                {
+                    throw new DecisionException("unable to find child", e);
+                }
+            }
+            set { AddChild(value, index); }
         }
     }
 
-    public class GraphEdge<TVertexType, TEdgeLabelType>
+    public class Edge<TVertexType, TEdgeLabelType>
         where TVertexType : IEquatable<TVertexType>
         where TEdgeLabelType : IEquatable<TEdgeLabelType>
     {
         public TEdgeLabelType LinkLabel { get; set; }
-        public GraphVertex<TVertexType, TEdgeLabelType> TargetVertex { get; set; }
+        public Vertex<TVertexType, TEdgeLabelType> TargetVertex { get; set; }
     }
 }
---
+++ bdd_tests/TreeTests.cs
@@ -0,0 +1,163 @@
+﻿using bdd;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace bdd_tests
+{
+    [TestClass]
+    public class TreeTests
+    {
+        [TestMethod]
+        public void CanCreateGraphVertex()
+        {
+            var sut = new Vertex<string, string>("hello world");
+            sut.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void CanLinkVertices()
+        {
+            var sut = new Vertex<string, string>("hello world");
+            sut.Children.Count().Should().Be(0);
+            sut.AddChild("node", "link");
+            sut.Children.Count().Should().Be(1);
+            sut.Children.First().LinkLabel.Should().Be("link");
+            sut.Children.First().TargetVertex.Value.Should().Be("node");
+        }
+
+        [TestMethod]
+        public void CanLinkVerticesUsingIndexer()
+        {
+            var sut = new Vertex<string, string>("hello world");
+            sut.Children.Count().Should().Be(0);
+            sut["edge"] = "vertex";
+            sut.Children.Count().Should().Be(1);
+            sut.Children.First().LinkLabel.Should().Be("edge");
+            sut.Children.First().TargetVertex.Value.Should().Be("vertex");
+        }
+
+        [TestMethod]
+        public void CanGetNodeValueViaIndexer()
+        {
+            var sut = new Vertex<string, string>("hello world");
+            sut["edge"] = "vertex";
+            sut["edge"].Should().Be("vertex");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DecisionException))]
+        public void HandlesInvalidAccessorProperly()
+        {
+            var sut = new Vertex<string, string>("hello world");
+            var dummy = sut["missing"];
+        }
+
+        [TestMethod]
+        public void CanCreateChain()
+        {
+            var sut = new Vertex<string, string>("vertex1");
+            sut["link1"] = "vertex2";
+            sut.Child("link1")["link2"] = "vertex3";
+            var v2 = sut.Child("link1");
+            v2.Should().NotBeNull();
+            var v3 = v2.Child("link2");
+            v3.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void CanHaveMultipleChildren()
+        {
+            var sut = new Vertex<string, string>("vertex1");
+            sut["e1"] = "v2";
+            sut["e2"] = "v3";
+            sut["e1"].Should().Be("v2");
+            sut["e2"].Should().Be("v3");
+        }
+
+        [TestMethod]
+        public void CanTestEquivalence()
+        {
+            var sut = new Vertex<string, string>("v1");
+            sut["e1"] = "v2";
+            sut["e2"] = "v3";
+
+            var sut2 = new Vertex<string, string>("v1");
+            sut2["e1"] = "v2";
+            sut2["e2"] = "v3";
+
+            sut.EquivalentTo(sut2).Should().Be(true);
+        }
+
+        [TestMethod]
+        public void CanTestEquivalenceRecursively()
+        {
+            var sut = new Vertex<string, string>("v1");
+            sut["e1"] = "v2";
+            sut.Child("e1")["e2"] = "v3";
+
+            var sut2 = new Vertex<string, string>("v1");
+            sut2["e1"] = "v2";
+            sut2.Child("e1")["e2"] = "v3";
+
+            sut.EquivalentTo(sut2).Should().Be(true);
+        }
+
+        [TestMethod]
+        public void CanTestNonequivalence()
+        {
+            var sut = new Vertex<string, string>("v1");
+            sut["e1"] = "v2";
+            sut["other"] = "values";
+
+            var sut2 = new Vertex<string, string>("v1");
+            sut2["e1"] = "v2";
+            sut2["e2"] = "v3";
+
+            sut.EquivalentTo(sut2).Should().Be(false);
+        }
+        [TestMethod]
+        public void CanTestNonequivalence2()
+        {
+            var sut = new Vertex<string, string>("v1");
+            sut["e1"] = "v2";
+            sut["other"] = "values";
+
+            var sut2 = new Vertex<string, string>("v1");
+
+            sut.EquivalentTo(sut2).Should().Be(false);
+        }
+        [TestMethod]
+        public void CanTestNonequivalence3()
+        {
+            var sut = new Vertex<string, string>("v1");
+
+            var sut2 = new Vertex<string, string>("v1");
+            sut2["e1"] = "v2";
+            sut2["e2"] = "v3";
+
+            sut.EquivalentTo(sut2).Should().Be(false);
+        }
+        [TestMethod]
+        public void CanTestNonequivalence4()
+        {
+            var sut = new Vertex<string, string>("v1");
+            var sut2 = new Vertex<string, string>("other");
+            sut.EquivalentTo(sut2).Should().Be(false);
+        }
+
+        [TestMethod]
+        public void CanTestNonequivalence5()
+        {
+            var sut = new Vertex<string, string>("v1");
+            sut["e1"] = "v2";
+            sut["e2"] = "different";
+
+            var sut2 = new Vertex<string, string>("v1");
+            sut2["e1"] = "v2";
+            sut2["e2"] = "v3";
+
+            sut.EquivalentTo(sut2).Should().Be(false);
+        }
+    }
+}
