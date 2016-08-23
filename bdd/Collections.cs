1ab591e7a6f8e332b185def83a5f76726fcde476
Andrew Matthews <andrew.matthews@unisuper.com.au>
2016-08-23 16:57:18 +1000

    misc changes (WIP)

---
+++ bdd/Collections.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bdd
+{
+    public class Tree<TNodeType, TEdgeLabelType>
+        where TNodeType : IEquatable<TNodeType>
+        where TEdgeLabelType : IEquatable<TEdgeLabelType>
+    {
+        /// <summary>
+        /// A link to the root node of the object graph that is the tree.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="Tree{TEdgeLabelType, TNodeType}"/> type is really just a holder 
+        /// for the actual tree which is the set of <see cref="TreeNode{TNodeType}"/> 
+        /// and <see cref="TreeEdge{TEdgeLabelType}"/>
+        /// </remarks>
+        public TreeEdge<TNodeType, TEdgeLabelType> Root { get; set; }
+        public IEnumerable<TreeNode<TNodeType, TEdgeLabelType>> Nodes
+        {
+            get
+            {
+                return GetAllNodes(Root.TargetNode);
+            }
+        }
+
+        private IEnumerable<TreeNode<TNodeType, TEdgeLabelType>> GetAllNodes(TreeNode<TNodeType, TEdgeLabelType> n)
+        {
+            yield return n;
+            var childNodes = n.Children.SelectMany(c => GetAllNodes(c.TargetNode));
+            foreach (var cn in childNodes)
+            {
+                yield return cn;
+            }
+        }
+    }
+
+
+    public class TreeNode<TNodeType, TEdgeLabelType>
+        where TNodeType : IEquatable<TNodeType>
+        where TEdgeLabelType : IEquatable<TEdgeLabelType>
+    {
+        public IEnumerable<TreeEdge<TNodeType, TEdgeLabelType>> Parents { get; }
+        public TNodeType Value { get; set; }
+        List<TreeEdge<TNodeType, TEdgeLabelType>> children = new List<TreeEdge<TNodeType, TEdgeLabelType>>();
+
+        public IEnumerable<TreeEdge<TNodeType, TEdgeLabelType>> Children
+        {
+            get
+            {
+                return children;
+            }
+        }
+
+        public TreeNode<TNodeType, TEdgeLabelType> AddChild(TNodeType n, TEdgeLabelType e)
+        {
+            if (children.Any(c => c.LinkLabel.Equals(e)))
+            {
+                throw new ApplicationException("Child link already exists.  Consider using Replace operation instead.");
+            }
+
+            var treeNode = new TreeNode<TNodeType, TEdgeLabelType>
+            {
+                Value = n
+            };
+            children.Add(new TreeEdge<TNodeType, TEdgeLabelType>
+                {
+                LinkLabel = e,
+                TargetNode = treeNode
+                });
+            return treeNode;
+        }
+    }
+
+    public class TreeEdge<TNodeType, TEdgeLabelType>
+        where TNodeType : IEquatable<TNodeType>
+        where TEdgeLabelType : IEquatable<TEdgeLabelType>
+    {
+        public TEdgeLabelType LinkLabel { get; set; }
+        public TreeNode<TNodeType, TEdgeLabelType> TargetNode { get; set; }
+    }
+}
--- bdd/DecisionTreeOptimiser.cs
+++ bdd/DecisionTreeOptimiser.cs
@@ -1,11 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace bdd
 {
+    interface IVisitor
+    {
+        void Visit(TerminalNode n);
+        bool StartVisit(BranchNode n);
+        void EndVisit(BranchNode n);
+    }
+    class Dispatcher
+    {
+        public static void Accept<TV>(TerminalNode n, TV visitor)
+          where TV : IVisitor
+        {
+            visitor.Visit(n);
+        }
+        public static void Accept<TV>(BranchNode n, TV visitor)
+          where TV : IVisitor
+        {
+            if (visitor.StartVisit(n))
+            {
+                foreach (var subpart in n.Branches.Values)
+                {
+                    if (subpart is TerminalNode)
+                    {
+                        Accept(subpart as TerminalNode, visitor);
+                    }
+                    else if (subpart is BranchNode)
+                    {
+                        Accept(subpart as BranchNode, visitor);
+                    }
+                }
+                visitor.EndVisit(n);
+            }
+        }
+    }
+
+    class BaseVisitor : IVisitor
+    {
+        public DecisionTree DT { get; set; }
+        public BaseVisitor(DecisionTree dt)
+        {
+            this.DT = dt;
+        }
+        public virtual void Visit(TerminalNode n) { }
+        public virtual bool StartVisit(BranchNode n)
+        {
+            return true;
+        }
+        public virtual void EndVisit(BranchNode n) { }
+    }
+
+    class TreePrunerVisitor : BaseVisitor
+    {
+        public TreePrunerVisitor(DecisionTree dt) : base(dt)
+        {
+        }
+        public override void Visit(TerminalNode n)
+        {
+            Debug.WriteLine("p2");
+        }
+        public override bool StartVisit(BranchNode n)
+        {
+            //  first try to remove this BN if it is redundant
+            var b = n.Branches.Select(x => x.Value);
+            if (b.Count() == 1 && b.First() is TerminalNode)
+            {
+                // there is only one outcome and it is terminal, so replace this node with the terminal node.
+                if (n.Parent == null)
+                {
+                    // this must be the root node of the DT, so replace it
+                    DT.TreeRoot = b.First();
+                    n.Branches = null;
+                    n.Symbol = null;
+                    // no need to continue with the visit any more, since the subtree has been eradicated.
+                    return false;
+                }
+                else
+                {
+                    n.Parent = b.First();
+                    n.Branches = null;
+                    n.Symbol = null;
+                    // no need to continue with the visit any more, since the subtree has been eradicated.
+                    return false;
+                }
+            }
+            if (Enumerable.All(b.Skip(1), x => x.Equals(b.First())))
+            {
+                // TODO: remove the node
+                // do something cool here...
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Remove a node from the tree by linking it's parent(s) to its children
+        /// </summary>
+        private void CollapseNode(Node n)
+        {
+
+        }
+        public override void EndVisit(BranchNode n)
+        {
+            Debug.WriteLine("<-c");
+        }
+    }
+
+
+
     public static class DecisionTreePruner
     {
         public static DecisionTree PruneTree(DecisionTree dtIn)
@@ -16,15 +123,6 @@ namespace bdd
 
         private static Node Prune(Node node)
         {
-            //if (node is BranchNode && node.IsRedundant())
-            //{
-            //    var bn = node as BranchNode;
-            //    var parent = bn.Parent as BranchNode;
-            //    foreach (var item in parent.)
-            //    {
-
-            //    }
-            //}
             throw new NotImplementedException();
         }
     }
